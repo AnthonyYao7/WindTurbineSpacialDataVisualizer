fda4530f313dc37e1302e5693df66aee6fcae05d
AnthonyYao7 <anthonyjyao@gmail.com>
2023-07-04 22:07:37 -0400

    initial commit

---
+++ ApplicationMain/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+#include "../k-d_tree/k-d_tree.h"
+
+using namespace std;
+#define COUNT 10
+
+typedef KD_Tree<double, 2>::KDTNode Node;
+
+void print2DUtil(Node* root, int space)
+{
+    // Base case
+    if (root == NULL)
+        return;
+
+    // Increase distance between levels
+    space += COUNT;
+
+    // Process right child first
+    print2DUtil(root->right, space);
+
+    // Print current node after space
+    // count
+    cout << endl;
+    for (int i = COUNT; i < space; i++)
+        cout << " ";
+    cout << (root->contains_point ? to_string(root->point[0]) + " " + to_string(root->point[1]) : "") << " " << root->plane << " " << root->dim << "\n";
+
+    // Process left child
+    print2DUtil(root->left, space);
+}
+
+// Wrapper over print2DUtil()
+void print2D(Node* root)
+{
+    // Pass initial space count as 0
+    print2DUtil(root, 0);
+}
+
+int main()
+{
+    vector<array<double, 2>> points = {{ 0.7726914985597692 , 0.7755390708672442 },//
+                                       { 0.7455338323568447 , 0.32162748467685165 },//
+                                       { 0.8090022694940407 , 0.09814634301417269 },//
+                                       { 0.962155906628329 , 0.3903571327081561 },//
+                                       { 0.09791475835241492 , 0.16639053361257072 },
+                                       { 0.43184344891685766 , 0.06854055267925951 },//
+                                       { 0.9549559743718948 , 0.24678514345444025 },
+                                       { 0.7176965024161037 , 0.49169206488839223 },//
+                                       { 0.2392238876896211 , 0.8041312208024288 },//
+                                       { 0.4178457900456979 , 0.6721843663643722 },};//
+
+    KD_Tree<double, 2> tree(points);
+
+    print2D(tree.get_root());
+    cout << endl;
+    array<array<double, 2>, 2> query = {{{0., 1.}, {0., 1.}}};
+    vector<array<double, 2>> queried = tree.range_query(query);
+
+    for (auto & i : queried)
+    {
+        cout << i[0] << " " << i[1] << endl;
+    }
+
+    return 0;
+}
---
+++ k-d_tree/k-d_tree.h
@@ -0,0 +1,148 @@
+//
+// Created by antho on 7/4/2023.
+//
+
+#ifndef DSA_P3_K_D_TREE_H
+#define DSA_P3_K_D_TREE_H
+
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <cstdint>
+#include <stack>
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+
+// implementation of k-d tree without support for insertion and deletion, i.e., only tree creation from some points and searching
+template <typename T, u64 K>
+class KD_Tree
+{
+public:
+
+    struct KDTNode
+    {
+        std::array<T, K> point;
+        bool contains_point;
+        u64 dim{};
+        T plane;
+        KDTNode* left;
+        KDTNode* right;
+
+        KDTNode(const std::array<T, K>& p) : point(p), left(nullptr), right(nullptr), contains_point(true) {}
+        KDTNode(u64 d, T pl) : dim(d) , plane(pl), contains_point(false) {}
+        KDTNode(const std::array<T, K>& p, u64 d, T pl) : point(p), dim(d), plane(pl), left(nullptr), right(nullptr), contains_point(true) {}
+    };
+
+private:
+
+    KDTNode* root = nullptr;
+
+public:
+
+    KDTNode* create_tree(
+            typename std::vector<std::array<T, K>>::iterator start,
+            typename std::vector<std::array<T, K>>::iterator stop,
+            u64 dim)
+    {
+        if (start == stop)
+            return nullptr;
+
+        if (start + 1 == stop)
+        {
+            return new KDTNode(*start, dim, (*start)[dim]);
+        }
+
+        // we sort to find the median and to avoid having to copy elements around
+        sort(start, stop, [&](const std::array<T, K> &a, const std::array<T, K> &b) { return a[dim] < b[dim]; });
+
+        u64 length = stop - start;
+        KDTNode* subtree_root = nullptr, *left = nullptr, *right = nullptr;
+
+        if (length % 2 == 0)
+        {
+            auto f = start + length / 2 - 1, s = start + length / 2;
+            subtree_root = new KDTNode(dim, ((*f)[dim] + (*s)[dim]) / 2.);
+            left = create_tree(start, s, (dim + 1) % K);
+            right = create_tree(s, stop, (dim + 1) % K);
+        }
+        else
+        {
+            auto m = start + length / 2;
+            subtree_root = new KDTNode(*m, dim, (*m)[dim]);
+            left = create_tree(start, m, (dim + 1) % K);
+            right = create_tree(m + 1, stop, (dim + 1) % K);
+        }
+
+        subtree_root->left = left;
+        subtree_root->right = right;
+        return subtree_root;
+    }
+
+    KD_Tree(const std::vector<std::array<T, K>>& points)
+    {
+        auto a = points;
+        root = create_tree(a.begin(), a.end(), 0);
+    }
+
+    KD_Tree() = default;
+
+    std::vector<std::array<T, K>> range_query(std::array<std::array<T, 2>, K>& range)
+    {
+        std::vector<std::array<T, K>> ret;
+        std::stack<KDTNode*> cs;
+        cs.push(root);
+        KDTNode* cur;
+        u64 cur_dim;
+
+        while (!cs.empty())
+        {
+            cur = cs.top();
+            cs.pop();
+
+            if (cur == nullptr)
+                continue;
+
+            cur_dim = cur->dim;
+
+            if (cur->plane <= range[cur_dim][1])
+            {
+                cs.push(cur->right);
+            }
+
+            if (cur->plane >= range[cur_dim][0])
+            {
+                cs.push(cur->left);
+            }
+
+            if (cur->contains_point)
+            {
+                for (int d = 0; d < K; d++)
+                {
+                    if (not (cur->point[d] >= range[d][0] and cur->point[d] <= range[d][1]))
+                        break;
+                    if (d == (K - 1))
+                        ret.push_back(cur->point);
+                }
+            }
+//            if (cur->contains_point and (cur->point[cur_dim] >= range[cur_dim][0] and cur->point[cur_dim] <= range[cur_dim][1]))
+//            {
+//                ret.push_back(cur->point);
+//            }
+        }
+
+        return ret;
+    }
+
+    KDTNode* get_root() { return root; }
+};
+
+
+#endif //DSA_P3_K_D_TREE_H
