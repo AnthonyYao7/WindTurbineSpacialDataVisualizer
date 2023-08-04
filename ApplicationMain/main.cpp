85ff28e8956e25ad489d1abced8c037dccfb1504
AnthonyYao7 <anthonyjyao@gmail.com>
2023-08-03 22:13:14 -0400

    Started app server

--- ApplicationMain/main.cpp
+++ ApplicationMain/main.cpp
@@ -2,69 +2,125 @@
 #include <vector>
 #include <array>
 #include <memory>
+#include <thread>
+#include <filesystem>
+#include <fstream>
+#include <unordered_map>
+
 #include "range_tree/range_tree.h"
 
-using namespace std;
-#define COUNT 10
+#include "SimpleWebServer/server_http.hpp"
+
+using HttpServer = SimpleWeb::Server<SimpleWeb::HTTP>;
+namespace fs = std::filesystem;
+
+inline std::string ui_path = "../../build";
 
 
-template <u64 K, u64 P>
-void print2DUtil(typename RangeTree<double, K, P>::RTNode* root, int space)
+std::unordered_map<std::string, std::string> mime_types = {
+        {"css", "text/css"},
+        {"html", "text/html"},
+        {"js", "application/javascript"},
+        {"svg", "image/svg+xml"},
+        {"ico", "image/vnd.microsoft.icon"},
+        {"json", "application/json"},
+        {"txt", "text/plain"},
+        {"png", "image/png"},
+};
+
+
+bool is_valid_file(const std::string& p)
 {
-    if (root == NULL)
-        return;
+    std::string concat = ui_path + p;
 
-    space += COUNT;
-
-    print2DUtil<K, P>(root->right, space);
-
-    cout << endl;
-    for (int i = COUNT; i < space; i++)
-        cout << " ";
-
-    cout << root->point[0] << " " << root->point[1] << endl;
-
-    print2DUtil<K, P>(root->left, space);
+    return fs::exists(concat) and fs::is_regular_file(concat);
 }
 
-template <u64 K, u64 P>
-void print2D(typename RangeTree<double, K, P>::RTNode* root)
+
+std::string get_content(const std::string& p)
 {
-    print2DUtil<K, P>(root, 0);
+    if (not is_valid_file(p))
+    {
+        std::cout << "file not found" << std::endl;
+        return "HTTP/1.1 404 NOT FOUND\r\nContent-Length: 0\r\n\r\n";
+    }
+
+    std::string concat = ui_path + p;
+
+    // Read the file into a string
+    std::ifstream file_stream(concat);
+    if (!file_stream)
+        exit(1);
+
+    std::string file_contents((std::istreambuf_iterator<char>(file_stream)),
+            std::istreambuf_iterator<char>());
+
+    // Close the file
+    file_stream.close();
+
+    int last = 0;
+
+    for (int i = p.size() - 1; i >= 0; --i)
+    {
+        if (p[i] == '.')
+        {
+            last = i;
+            break;
+        }
+    }
+
+    std::string file_extension = p.substr(last + 1);
+
+    std::string file_mime_type = mime_types[file_extension];
+
+    return "HTTP/1.1 200 OK\r\nContent-Length: " + std::to_string(file_contents.length()) + "\r\nContent-Type: " + file_mime_type + "\r\n\r\n" + file_contents;
 }
+
 
 int main()
 {
-    vector<array<double, 2>> points = {{0.2, 0.2}, {0.8, 0.4}, {0.5, 0.5}, {0.2, 0.4}, {0.2, 0.8}};
-    array<array<double, 2>, 2> query = {{{0., 1.}, {0.3, 0.6}}};
+    HttpServer server;
+    server.config.port = 8080;
 
-    auto tree = make_unique<RangeTree<double, 2>>(points.begin(), points.end());
+    server.resource["^/static/css/[^\\/]+$"]["GET"] = [] (std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) {
+        std::string p = request->path;
 
-    print2D<2, 2>(tree->get_root());
+        *response << get_content(p);
+    };
 
-    queue<RangeTree<double, 2>::RTNode*> q;
-    q.push(tree->get_root());
+    server.resource["^/static/js/[^\\/]+$"]["GET"] = [] (std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) {
+        std::string p = request->path;
 
-    while (not q.empty())
-    {
-        auto cur = q.front();
-        q.pop();
+        *response << get_content(p);
+    };
 
-        if (cur == nullptr)
-            continue;
+    server.resource["^/static/media/[^\\/]+$"]["GET"] = [] (std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) {
+        std::string p = request->path;
 
-        cout << cur->point[0] << " " << cur->point[1] << "sugma" << endl;
-        print2D<1, 2>(cur->orthogonal->get_root());
+        *response << get_content(p);
+    };
 
-        q.push(cur->left);
-        q.push(cur->right);
-    }
+    server.resource["^/[^\\/]+$"]["GET"] = [] (std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) {
+        std::string p = request->path;
 
-    auto results = tree->range_query(query);
+        std::cout << "HI" << std::endl;
 
-    cout << "Num results: " << results.size() << endl;
+        *response << get_content(p);
+    };
 
+    server.resource["^/data/[^\\/]+$"]["GET"] = [] (std::shared_ptr<HttpServer::Response> response, std::shared_ptr<HttpServer::Request> request) {
+        std::string p = request->path;
 
+        std::cout << "HI" << std::endl;
+
+        *response << get_content(p);
+    };
+
+    std::thread server_thread([&server]() {
+        server.start();
+    });
+
+    server_thread.join();
 
     return 0;
 }
--- src/Map.js
+++ src/Map.js
@@ -8,73 +8,72 @@ import leafletPip from 'leaflet-pip';
 
 function MyMap() {
     const [drawControl, setDrawControl] = useState(null);
-    
-    const MyComponent = () => {
-      const map = useMap();
-  
-      useEffect(() => {
-        if (!drawControl) {
-          const control = new L.Control.Draw({
-            draw: {
-              polyline: false,
-              polygon: false,
-              circle: false,
-              marker: false,
-              rectangle: true,
-            },
-          });
-          setDrawControl(control);
-          map.addControl(control);
-        }
-      }, [map, drawControl]);
-  
-      useEffect(() => {
-        axios.get(process.env.PUBLIC_URL + '/data/custom.geo.json')
-        .then(res => {
-          const geoJsonLayer = L.geoJSON(res.data);
-          geoJsonLayer.addTo(map);
-  
-          map.on(L.Draw.Event.CREATED, function (e) {
-            const type = e.layerType;
-            const layer = e.layer;
-  
-            if (type === 'rectangle') {
-              const latlngs = layer.getLatLngs();
-              console.log(`${type} corners: `, latlngs);
-  
-              latlngs[0].forEach(point => {
-               // console.log("Point:", point);  // print the whole point object
 
-                const result = leafletPip.pointInLayer([point.lng, point.lat], geoJsonLayer, true);
-                if(result.length === 0) {
-                  console.log("Point is outside US: ", point);
-                }
-                else {
-                  console.log("Point is inside US: ", point);
-                }
-              });
+    const MyComponent = () => {
+        const map = useMap();
+        console.log("hi")
+        useEffect(() => {
+            if (!drawControl) {
+                const control = new L.Control.Draw({
+                    draw: {
+                        polyline: false,
+                        polygon: false,
+                        circle: false,
+                        marker: false,
+                        rectangle: true,
+                    },
+                });
+                setDrawControl(control);
+                map.addControl(control);
             }
-          });
-        });
-      }, [map]);
-  
-      return null;
+        }, [map, drawControl]);
+
+        useEffect(() => {
+            axios.get(process.env.PUBLIC_URL + '/data/UnitedStatesPolygon.geo.json')
+                .then(res => {
+                    const geoJsonLayer = L.geoJSON(res.data);
+                    geoJsonLayer.addTo(map);
+
+                    map.on(L.Draw.Event.CREATED, function (e) {
+                        const type = e.layerType;
+                        const layer = e.layer;
+
+                        if (type === 'rectangle') {
+                            const latlngs = layer.getLatLngs();
+                            console.log(`${type} corners: `, latlngs);
+
+                            latlngs[0].forEach(point => {
+
+                                const result = leafletPip.pointInLayer([point.lng, point.lat], geoJsonLayer, false);
+
+                                if(result.length === 0) {
+                                    console.log("Point is outside US: ", point);
+                                }
+                                else {
+                                    console.log("Point is inside US: ", point);
+                                }
+                            });
+                        }
+                    });
+                });
+        }, [map]);
+
+        return null;
     };
-  
+
     return (
-      <MapContainer
-        center={[39.50, -98.35]}
-        zoom={4}
-        style={{ height: "100vh", width: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MyComponent />
-      </MapContainer>
+        <MapContainer
+            center={[39.50, -98.35]}
+            zoom={4}
+            style={{ height: "100vh", width: "100%" }}
+        >
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <MyComponent />
+        </MapContainer>
     );
-  }
-  
-  export default MyMap;
-  
+}
+
+export default MyMap;
