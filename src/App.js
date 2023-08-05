2646f394d47f7d5ae0a953201294dcfb12d4cefb
suurgatio <serajsaboungi@ufl.edu>
2023-08-04 20:23:58 -0400

    updated code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
 import MyMap from './Map';
+import axios from 'axios';
+
 
 function App() {
+  const [tableData, setTableData] = useState(null);
+
+  // This useEffect hook will fetch the data when the component is mounted.
+  useEffect(() => {
+    // Use axios or fetch to get the data from your server or database.
+    // This is just a placeholder and should be replaced with your actual API call.
+    axios.get("/api/your-endpoint")
+      .then((response) => {
+        setTableData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="main-title">
           Wind Turbine Location Visualizer
         </p>
+        <p className="instructions">
+          Select the rectangle tool, and draw a region anywhere inside the United States. The data for all the wind turbines within your selected region will then be displayed for you below the map.
+        </p>
         <MyMap />
-        <a>
-          Click anywhere on the map to set the latitude + longitude.
+        <a className="main-title">
+          Wind Turbine Data
         </a>
+        {tableData && 
+          <table>
+            <thead>
+              <tr>
+                <th>State</th>
+                <th>County</th>
+                <th>Year</th>
+                <th>Turbine's Capacity</th>
+                <th>Turbine's Hub Height</th>
+                <th>Turbine's Rotor Diameter</th>
+                <th>Swept Area</th>
+                <th>Turbine's Total Height</th>
+                <th>Project's Capacity</th>
+                <th>Number of Turbines in Project</th>
+                <th>Turbine's Latitude</th>
+                <th>Turbine's Longitude</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.state}</td>
+                  <td>{item.county}</td>
+                  <td>{item.year}</td>
+                  <td>{item.turbineCapacity}</td>
+                  <td>{item.turbineHubHeight}</td>
+                  <td>{item.turbineRotorDiameter}</td>
+                  <td>{item.sweptArea}</td>
+                  <td>{item.turbineTotalHeight}</td>
+                  <td>{item.projectCapacity}</td>
+                  <td>{item.numberOfTurbinesInProject}</td>
+                  <td>{item.turbineLatitude}</td>
+                  <td>{item.turbineLongitude}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        }
       </header>
     </div>
   );
+
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -5,76 +5,105 @@ import 'leaflet-draw/dist/leaflet.draw.css';
 import 'leaflet-draw';
 import axios from 'axios';
 import leafletPip from 'leaflet-pip';
+import './Map.css';
+
+let drawnLayers = new L.FeatureGroup();
 
 function MyMap() {
-    const [drawControl, setDrawControl] = useState(null);
-    
-    const MyComponent = () => {
-      const map = useMap();
-  
-      useEffect(() => {
-        if (!drawControl) {
-          const control = new L.Control.Draw({
-            draw: {
-              polyline: false,
-              polygon: false,
-              circle: false,
-              marker: false,
-              rectangle: true,
-            },
-          });
-          setDrawControl(control);
-          map.addControl(control);
-        }
-      }, [map, drawControl]);
-  
-      useEffect(() => {
-        axios.get(process.env.PUBLIC_URL + '/data/custom.geo.json')
-        .then(res => {
-          const geoJsonLayer = L.geoJSON(res.data);
-          geoJsonLayer.addTo(map);
-  
-          map.on(L.Draw.Event.CREATED, function (e) {
-            const type = e.layerType;
-            const layer = e.layer;
-  
-            if (type === 'rectangle') {
-              const latlngs = layer.getLatLngs();
-              console.log(`${type} corners: `, latlngs);
-  
-              latlngs[0].forEach(point => {
-               // console.log("Point:", point);  // print the whole point object
+  const [drawControl, setDrawControl] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-                const result = leafletPip.pointInLayer([point.lng, point.lat], geoJsonLayer, true);
-                if(result.length === 0) {
-                  console.log("Point is outside US: ", point);
-                }
-                else {
-                  console.log("Point is inside US: ", point);
-                }
-              });
-            }
-          });
+  const MyComponent = () => {
+    const map = useMap();
+
+    useEffect(() => {
+      if (!drawControl) {
+        const control = new L.Control.Draw({
+          draw: {
+            polyline: false,
+            polygon: false,
+            circle: false,
+            marker: false,
+            rectangle: true,
+          },
         });
-      }, [map]);
-  
-      return null;
-    };
-  
-    return (
-      <MapContainer
-        center={[39.50, -98.35]}
-        zoom={4}
-        style={{ height: "100vh", width: "100%" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <MyComponent />
-      </MapContainer>
-    );
-  }
-  
-  export default MyMap;
-  
+        setDrawControl(control);
+        map.addControl(control);
+      }
+    }, [map, drawControl]);
+
+    useEffect(() => {
+      axios.get(process.env.PUBLIC_URL + '/data/custom.geo.json')
+      .then(res => {
+        const geoJsonLayer = L.geoJSON(res.data);
+        geoJsonLayer.addTo(map);
+
+        // Remove all previous layers when the user starts a new drawing
+        map.on(L.Draw.Event.DRAWSTART, function () {
+          drawnLayers.eachLayer(function (layer) {
+            map.removeLayer(layer);
+          });
+          drawnLayers = new L.FeatureGroup();
+        });
+
+        map.on(L.Draw.Event.CREATED, function (e) {
+          const type = e.layerType;
+          const layer = e.layer;
+          setErrorMessage('');
+        
+          if (type === 'rectangle') {
+            const latlngs = layer.getLatLngs();
+            let isInsideUS = false;
+        
+            latlngs[0].forEach(point => {
+              const result = leafletPip.pointInLayer([point.lng, point.lat], geoJsonLayer, true);
+              if(result.length !== 0) {
+                isInsideUS = true;
+              }
+            });
+        
+            if (!isInsideUS) {
+              setErrorMessage('All points of the drawn region fall outside the US. Please redraw the region.');
+            }
+          }
+        
+          // Add the drawn layer to the map and to the drawnLayers feature group
+          drawnLayers.addLayer(layer);
+          map.addLayer(layer);
+        });
+        
+      });
+
+      // Clean up when component unmounts
+      return () => {
+        map.off(L.Draw.Event.DRAWSTART);
+        map.off(L.Draw.Event.CREATED);
+      };
+
+    }, [map]);
+
+    return null;
+  };
+
+  return (
+    <MapContainer
+      center={[39.50, -98.35]}
+      zoom={4}
+      style={{ height: "100vh", width: "100%" }}
+      dragging={false}
+      zoomControl={true}
+      touchZoom={true}
+      doubleClickZoom={true}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MyComponent />
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
+    </MapContainer>
+  );
+}
+
+export default MyMap;
