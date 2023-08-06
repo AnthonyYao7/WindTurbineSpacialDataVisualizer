d89198cd76d277eabc0fbdc5d88bd24e709116e2
suurgatio <serajsaboungi@ufl.edu>
2023-08-05 20:06:17 -0400

    updated code

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         axios.post("http://localhost:8080/query", {coordinates: bounds.join(",")})
             .then((response) => {
                 setTableData(response.data);
+                console.log(response.data);
             })
             .catch((error) => {
                 console.log(error);
@@ -25,52 +26,61 @@ function App() {
 }, [bounds]);
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="main-title">
-          Wind Turbine Location Visualizer
-        </p>
-        <p className="instructions">
-          Select the rectangle tool, and draw a region anywhere inside the United States. The data for all the wind turbines within your selected region will then be displayed for you below the map.
-        </p>
-        <MyMap bounds={bounds} setBounds={setBounds} />
-        <a className="main-title">
-          Wind Turbine Data
-        </a>
+return (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p className="main-title">
+        Wind Turbine Location Visualizer
+      </p>
+      <p className="instructions">
+        Select the rectangle tool, and draw a region anywhere inside the United States. The data for all the wind turbines within your selected region will then be displayed for you below the map.
+      </p>
+      <MyMap bounds={bounds} setBounds={setBounds} />
+      <a className="main-title">
+        Wind Turbine Data
+      </a>
+      <div className="table-container">
         {tableData && 
-  <table>
-    <thead>
-      <tr>
-        <th>Earliest built</th>
-        <th>Latest built</th>
-        <th>Average capacity</th>
-        <th>Tallest turbine</th>
-        <th>Shortest turbine</th>
-        <th>Average rotor diameter</th>
-        <th>Turbine density</th>
-        <th>Area of selection</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{tableData["Earliest built"]}</td>
-        <td>{tableData["Latest built"]}</td>
-        <td>{tableData["Average capacity"]}</td>
-        <td>{tableData["Tallest turbine"]}</td>
-        <td>{tableData["Shortest turbine"]}</td>
-        <td>{tableData["Average rotor diameter"]}</td>
-        <td>{tableData["Turbine density"]}</td>
-        <td>{tableData["Area of selection"]}</td>
-      </tr>
-    </tbody>
-  </table>
-}
-      </header>
-    </div>
-  );
+          <table>
+            <thead>
+              <tr>
+                <th>Number of Turbines</th>
+                <th>Area of selection (sq. mi.)</th>
+                <th>Turbine density</th>
+                <th>Earliest built</th>
+                <th>Latest built</th>
+                <th>Tallest turbine (m)</th>
+                <th>Shortest turbine (m)</th>
+                <th>Average capacity (KW)</th>
+                <th>Average rotor diameter (m)</th>
+                <th>Range tree query took (ms)</th>
+                <th>K-d tree query took (ms)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{tableData["Number of Turbines"]}</td>
+                <td>{tableData["Area of selection"]}</td>
+                <td>{tableData["Turbine density"]}</td>
+                <td>{tableData["Earliest built"]}</td>
+                <td>{tableData["Latest built"]}</td>
+                <td>{tableData["Tallest turbine"]}</td>
+                <td>{tableData["Shortest turbine"]}</td>
+                <td>{tableData["Average capacity"]}</td>
+                <td>{tableData["Average rotor diameter"]}</td>
+                <td>{tableData["Range tree query took"]}</td>
+                <td>{tableData["K-d tree query took"]}</td>
+              </tr>
+            </tbody>
+          </table>
+        }
+      </div>
+    </header>
+  </div>
+);
+
+
 
 }
-
 export default App;
